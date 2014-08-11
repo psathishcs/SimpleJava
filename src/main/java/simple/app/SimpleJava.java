919e8c6d7e056dfb2edaf5f92081347a9ae9fc94
Sathish Kumar <p.sathish.cs@gmail.com>
2014-08-11 15:16:41 -0400

    Inital SimpleJavaApp

---
+++ src/main/java/simple/app/SimpleJava.java
@@ -0,0 +1,11 @@
+package simple.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleJava {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
