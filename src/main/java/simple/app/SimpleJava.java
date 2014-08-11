2ff4bd79eda19ba1d92aca963a76f917f5de6d0c
Sathish Kumar <p.sathish.cs@gmail.com>
2014-08-11 19:24:40 -0400

    Adding Sonar

--- src/main/java/simple/app/SimpleJava.java
+++ src/main/java/simple/app/SimpleJava.java
@@ -8,5 +8,6 @@ public class SimpleJava {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println("Using Git");
+        System.out.println("Using Sonar Analyser!!");
     }
 }
