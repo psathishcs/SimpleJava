104e512bca4b6c86a51e56547098c50937f7b379
Sathish Kumar <p.sathish.cs@gmail.com>
2014-08-11 15:41:45 -0400

    Client asked me the change the file:w

--- src/main/java/simple/app/SimpleJava.java
+++ src/main/java/simple/app/SimpleJava.java
@@ -7,5 +7,6 @@ package simple.app;
 public class SimpleJava {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println("Using Git");
     }
 }
